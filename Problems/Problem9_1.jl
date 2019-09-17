0544f0188f665c99a549072f20577cd4a38c989c
Akshay Hiregoudar <51836938+akshayhiregoudar@users.noreply.github.com>
2019-09-17 00:32:28 -0500

    Add files via upload

---
+++ Problems/Problem9_1.jl
@@ -0,0 +1,12 @@
+
+A=[0 2;0 0];
+t=(-5,0.5,5);
+X1=repmat([t;]',length(t));
+X2=repmat([t;],1,length(t));
+#Let exp(A*t)=E
+E=[1 2.*t;0 1];
+
+using PyPlot
+
+quiver(X1,X2,E,1)
+plot(X[:,1],X[:,2],label="Problem_9_1",colour="r")
